93e2f7870d246b446463814479e289cf3172a948
Daniel Weber <dantech88@yahoo.com>
2016-06-08 23:54:11 -0500

    Update event_handler.py

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -35,7 +35,7 @@ class RtmEventHandler(object):
 
     def _handle_message(self, event):
         # Filter out messages from the bot itself
-        if not self.clients.is_message_from_me(event['user']):
+        if event['user'] and not self.clients.is_message_from_me(event['user']):
 
             msg_txt = event['text']
             logger.info("MSG text:" + msg_txt)
